e2fc46fd73aad9765c3a8ec830354ce25bc8bd8d
mikamo3 <kamo3proj@gmail.com>
2020-02-17 15:28:15 +0000

    fix role

--- run_test.sh
+++ run_test.sh
@@ -3,4 +3,4 @@ TAGS="init"
 if [[ "$1" != "" ]]; then
   TAGS="$1"
 fi
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_test.yml playbook.yml -v --tags=$TAGS
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_test.yml playbook.yml -v --tags=$TAGS --ask-become-pass
