bf3c3af616525ac12f1f54df8b679e7ec6384a3b
mikamo3 <kamo3proj@gmail.com>
2020-02-18 15:51:44 +0000

    :bug: (snapper) exclude run in chroot

--- run_init.sh
+++ run_init.sh
@@ -1,2 +1,2 @@
 ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml --tags=init && \
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml --skip-tags=excludeChroot
