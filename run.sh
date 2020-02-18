23dfd93c23e2502197c7333e18475f0cf27fd443
mikamo3 <kamo3proj@gmail.com>
2020-02-18 17:01:55 +0000

    :bug: (run.sh) fix script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-TAGS="init"
 if [[ "$1" != "" ]]; then
-  TAGS="$1"
+  TAGS="--tags=$1"
 fi
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml -v --tags=$TAGS --ask-become-pass
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml -v $TAGS --ask-become-pass
